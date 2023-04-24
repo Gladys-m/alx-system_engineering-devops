4ccf40aac4d445059dfa11ff67f292852e54dc82
Gladys-m <gladysmitambo@gmail.com>
2023-04-24 17:35:17 +0300

    using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,12 @@
 # Setting up client SSH configuration file
-include stdlib
-
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no'
+  line   => '    PasswordAuthentication no',
 }
+
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton'
+  line   => '    IdentityFile ~/.ssh/holberton',
 }
